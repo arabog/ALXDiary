aaed5a615b5a48e605e71fe0eec4c6c3a3b27c68
HUGB <arababs2015@gmail.com>
2022-05-12 10:06:52 +0100

    ALX start  2

---
+++ Wk1/info.js
@@ -0,0 +1,44 @@
+/*
+Continuous integration and continuous delivery (CI/CD) pipelines
+
+Project 1 - Deploy Static Website on AWS
+In this project, you will deploy a static website to AWS. 
+First, you will create an S3 bucket, 
+configure the bucket for website hosting, and 
+secure it using IAM policies. 
+
+Next, you will upload the website files to your bucket and 
+speed up content delivery using AWS’s content distribution 
+network service, CloudFront. 
+
+Lastly, you will access your website in a browser using 
+the unique S3 endpoint.
+
+
+Project 2 - Deploy a high-availability web app using CloudFormation
+In this project, you’ll deploy web servers for a highly available web app 
+using CloudFormation. You will write the code that creates and deploys 
+the infrastructure and application for an Instagram-like app from the 
+ground up. You will begin with deploying the networking components 
+followed by servers, security roles and software. 
+
+
+Project 3 - Build CI/CD Pipelines, Monitoring & Logging
+In this course, you’ll learn the process of taking software from source 
+code to deployment and beyond. You’ll learn about automated testing, 
+choosing the right deployment strategy for your business needs and 
+deploying an appropriate CI/CD pipeline. You’ll also learn about 
+monitoring and logging to ensure that your application is running at 
+peak performance and stays that way.
+
+You’ll also learn to manage and make changes to your servers in 
+an automated way, using Ansible, a leading Configuration 
+Management tool.
+
+
+
+
+
+
+
+*/ 
