65c025ad501763aaafb3931e46ddb2ad7767506d
HUGB <arababs2015@gmail.com>
2022-05-12 12:07:27 +0100

    ALX start  3

--- Wk1/info.js
+++ Wk1/info.js
@@ -36,6 +36,62 @@ an automated way, using Ansible, a leading Configuration
 Management tool.
 
 
+Project 4 - Operationalize a Machine Learning Microservice API
+In this project, you will continue your work on operationalizing 
+microservices by deploying an elastic and fault-tolerant Machine 
+Learning inference API using Kubernetes. 
+
+You will validate your design by load testing the service and 
+verifying the application architecture performs as designed.
+
+
+Project 5 - Capstone Project
+In the capstone project, each project is unique to the student. 
+You’ll build a CI/CD pipeline for a microservices application 
+for different deployment strategies. Students define the scope 
+of the project and select the right deployment strategy based 
+on different business requirements.
+
+
+Meet Your Instructors
+Course 1 - Cloud Fundamentals           
+Kesha Williams, Software Engineering Manager at Chick-fil-A
+
+Course 2 - Deploy Infrastructure as Code (IAC)
+Carlos Rivas, Senior Solutions Architect at Infiniti Consulting
+
+Course 3 - Build CI/CD Pipelines, Monitoring & Logging
+Byron Sommardahl, Chief Technology Officer, Acklen Avenue
+
+Course 4 - Microservices at Scale using AWS & Kubernetes
+Noah Gift, Lecturer at UC Berkeley School of Information
+
+He is the author of Python DevOps and Pragmatic AI: An 
+Introduction to Cloud-Computing.
+
+
+Questions:
+You will find this article by Justin Arbuckle on ELSA 
+(Event, Language, Structure, Agency) change model 
+an interesting read.
+
+https://classroom.udacity.com/nanodegrees/nd9991-alg-t2/parts/ad6c5933-2445-411c-a268-9c91c18ca883/modules/1b72245e-6ec5-49e4-b70e-62d47b96d936/lessons/f6c1e089-d397-44bb-a02b-6f2af82a54af/concepts/b258a844-7157-4dfd-b33c-075d3e767fa4#
+
+
+What we create is a reflection of our
+communication pattern
+
+
+Getting Help
+As questions come up during this course, click on the Help 
+button on the left sidebar of your classroom. You’ll see four 
+options, each for a separate type of support:
+
+https://classroom.udacity.com/nanodegrees/nd9991-alg-t2/parts/ad6c5933-2445-411c-a268-9c91c18ca883/modules/1b72245e-6ec5-49e4-b70e-62d47b96d936/lessons/0536a891-7664-44a4-a2e6-ca153c830fe6/concepts/7fcfb956-2161-4a66-b4ec-eefea7f9869b
+
+
+CLOUD JOBS
+https://classroom.udacity.com/nanodegrees/nd9991-alg-t2/parts/ad6c5933-2445-411c-a268-9c91c18ca883/modules/1b72245e-6ec5-49e4-b70e-62d47b96d936/lessons/4b30ec95-9a3c-4467-ae8e-16cae6084262/concepts/8821bd4c-9df7-42c6-aa53-290ee833c404
 
 
 
