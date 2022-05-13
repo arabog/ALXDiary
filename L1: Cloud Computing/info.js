b377779e1beb56f15aca6b01df36fd2509f15e1b
HUGB <arababs2015@gmail.com>
2022-05-13 06:02:14 +0100

    alx cloud  2

--- L1: Cloud Computing/info.js
+++ L1: Cloud Computing/info.js
@@ -1,6 +1,44 @@
 /*
+Cloud Computing
+Cloud Computing is the delivery of IT resources over the Internet. 
+
+It allows you to manage: (Databases, Compute Power, Application, Security)
+Storage services like databases
+Servers, compute power, networking
+Analytics, artificial intelligence, augmented reality
+Security services for data and applications
 
 
+Characteristics of Cloud Computing
+Pay as you go - You pay only for what you use and only when your code runs.
+Autoscaling - The number of active servers can grow or shrink based on demand.
+Serverless - Allows you to write and deploy code without having to worry 
+about the underlying infrastructure.
+
+
+Types of Cloud Computing:
+Infrastructure-as-a-Service (IaaS): The provider supplies virtual server 
+instances, storage, and mechanisms for you to manage servers.
+An IaaS provides underlying infrastructure
+
+Platform-as-a-Service (PaaS): A platform of development tools hosted 
+on a provider's infrastructure.
+PaaS is a platform of both hardware and software tools such as operating 
+systems and database management and development tools.
+
+
+Software-as-a-Service (SaaS): A software application that runs over the 
+Internet and is managed by the service provider.
+
+
+
+
+Match the provider with the type of service:
+
+TYPE                                                                  PROVIDER
+Platform-as-a-Service (PaaS)                                GoDaddy
+Infrastructure-as-a-Service (IaaS)                          Rackspace
+Software-as-a-Service (SaaS)                                Gmail
 
 
 */
