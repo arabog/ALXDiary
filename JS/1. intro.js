8e524444b9820c087051ef2370ba7b4c94c6b440
HUGB <arababs2015@gmail.com>
2022-05-14 23:44:43 +0100

    devops 4

---
+++ JS/1. intro.js
@@ -0,0 +1,19 @@
+/*
+ctrl + shift + j === developer console
+
+document.getElementsByTagName("h1")[0].style.color = "#ff0000";
+
+document.body.addEventListener('click', function () {
+          const myParent = document.getElementsByTagName("h1")[0]; 
+
+          const myImage = document.createElement("img");
+          myImage.src = 'https://thecatapi.com/api/images/get?format=src&type=gif';
+
+          myParent.appendChild(myImage);
+          myImage.style.marginLeft = "160px";
+});
+
+All major browsers come with built-in JavaScript engines that 
+allow browsers to run and execute JavaScript code. e.g console.log()
+
+*/
