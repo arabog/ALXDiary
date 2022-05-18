2c080bab56f29ed8e0e8b9fca2a87a4a15c815a7
HUGB <arababs2015@gmail.com>
2022-05-18 15:24:05 +0100

    L3 2

--- Cloud/L3: Storage And ContentDelivery/info.js
+++ Cloud/L3: Storage And ContentDelivery/info.js
@@ -11,7 +11,6 @@ availability: addresses hw quickly u can access ur data,
 scalability: allows applicatns running in ur environmt to alw
 meet demand seamlessly
 
-
 Storage & Database Services
 Amazon Simple Storage Service (Amazon S3)
 Amazon Simple Storage Service (Amazon S3) Glacier
@@ -21,6 +20,29 @@ Redshift
 ElastiCache
 Neptune
 Amazon DocumentDB
+
+-: S3 & S3 Glacier
+Amazon Simple Storage Service (or S3) is an object storage system in the cloud.
+Put data u r nt often to use in S3 Glacier e.g annual statement report
+
+S3 Glacier is a secure, durable, and low-cost storage class for data archiving.
+
+Storage Classes
+S3 offers several storage classes, which are different data access levels for your 
+data at certain price points.
+
+Use Cases:
+Static websites
+Content delivery
+Backup and recovery
+Archiving and big data
+Applicatn data
+Hybrid cloud storage
+
+
+
+
+
 
 
 */ 
