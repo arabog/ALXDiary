ee2f272fa972f96e54147165f37a5c7e77742e2f
HUGB <arababs2015@gmail.com>
2022-05-18 18:03:00 +0100

    L3  4

--- Cloud/L3: Storage And ContentDelivery/info.js
+++ Cloud/L3: Storage And ContentDelivery/info.js
@@ -39,6 +39,59 @@ Archiving and big data
 Applicatn data
 Hybrid cloud storage
 
+-: DynamoDB
+DynamoDB is a NoSQL document database service that is fully managed. 
+Unlike traditional databases, NoSQL databases, are schema-less. 
+Schema-less simply means that the database doesn't contain a fixed (or 
+rigid) data structure.
+
+Tips
+DynamoDB is found under the Database section on the AWS Management Console.
+DynamoDB can handle more than 10 trillion requests per day.
+DynamoDB is serverless as there are no servers to provision, patch, or manage.
+DynamoDB supports key-value and document data models.
+DynamoDB synchronously replicates data across three AZs in an AWS Region.
+DynamoDB supports GET/PUT operations using a primary key.
+
+Each row in DynamoDB table is called a document
+Data is stored in DynamoDB in JSON or JSON-like format.
+
+In the shared responsibility model, when working with DynamoDB, what 
+is NOT your responsibility? Pick two
+Patch or upgrade d db
+Provisn or manage servers
+
+AWS is responsible for patching or upgrading the database. They are 
+also responsible for provisioning or managing servers.
+
+According to AWS:
+DynamoDB is a managed NoSQL database service that automatically 
+manages the hardware provisioning, setup, configuration, replication, 
+software patching, and cluster scaling.
+
+https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.html
+
+Global Tables - When you set up a table as a global table, that table can 
+exist in two or more AWS regions with automatic replication.
+
+Backups - A backup helps in restoring a Point-in-time state. DynamoDB 
+maintains continuous backups of your table for the last 35 days.
+
+External Resource
+AWS has a very useful tutorial to get started with DynamoDB. It has the
+following guided sections:
+
+Step 1: Create a NoSQL Table
+Step 2: Add Data to the NoSQL Table
+Step 3: Query the NoSQL Table
+Step 4: Delete an Existing Item
+Step 5: Delete a NoSQL Table
+
+Relational Database Service (RDS)
+
+
+
+
 
 
 
