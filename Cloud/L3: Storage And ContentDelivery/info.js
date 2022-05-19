2408d6a4bdac5e11c6e572be8cec43c347951f4e
HUGB <arababs2015@gmail.com>
2022-05-19 11:29:12 +0100

    l3  6

--- Cloud/L3: Storage And ContentDelivery/info.js
+++ Cloud/L3: Storage And ContentDelivery/info.js
@@ -175,6 +175,45 @@ low latency
 decreased server load
 better user experience
 
+-: Cloud Front
+CloudFront is used as a global content delivery network (CDN). 
+Cloud Front speeds up the delivery of your content through 
+Amazon's worldwide network of mini-data centers called Edge 
+Locations.
+
+CloudFront works with other AWS services, as shown below, 
+as an origin source for your application:
+
+Amazon S3
+Elastic Load Balancing
+Amazon EC2
+Lambda@Edge
+AWS Shield
+
+Tips
+CloudFront is found under the Networking & Content Delivery 
+section on the AWS Management Console.
+Amazon countinously adds new Edge Locations.
+CloudFront ensures that end-user requests are served from the 
+closest edge location.
+CloudFront works with non-AWS origin sources.
+You can use GeoIP blocking to serve content (or not serve content) 
+to specific countries.
+Cache control headers determine how frequently CloudFront needs 
+to check the origin for an updated version your file.
+The maximum size of a single file that can be delivered through 
+Amazon CloudFront is 20 GB.
+
+What happens if the content is not found in the edge location when 
+a user requests your application?
+Files are pulled from the origin and then stored in the cache at 
+the edge location.
+
+What is one reason you'd want to use Cloud Front?
+Cloud Front is used to stream content more efficiently.
+
+
+
 
 
 */ 
