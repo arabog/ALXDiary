b9492957b64cb6a58e330eb5c32cd6ea2ba8e5b8
HUGB <arababs2015@gmail.com>
2022-05-19 11:22:34 +0100

    l3  5

--- Cloud/L3: Storage And ContentDelivery/info.js
+++ Cloud/L3: Storage And ContentDelivery/info.js
@@ -162,6 +162,20 @@ processing day-to-day transactions
 A data warehouse is used for reporting and data analysis
 
 
+-: Content Delivery In The Cloud
+A Content Delivery Network (or CDN) speeds up delivery of your 
+static and dynamic web content by caching content in an Edge 
+Location close to your user base.
+
+
+
+Benefits
+The benefits of a CDN includes:
+low latency
+decreased server load
+better user experience
+
+
 
 */ 
 
