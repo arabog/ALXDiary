f965f5585c17f3b24114d39c25a4930f0daaeaa8
HUGB <arababs2015@gmail.com>
2022-05-20 08:31:53 +0100

    L5  2

--- Cloud/L2/L5: Networking and Elasticity/info.js
+++ Cloud/L2/L5: Networking and Elasticity/info.js
@@ -17,8 +17,37 @@ What is the address of a computer called?
 The IP address is a long string of numbers that represents a 
 computer's location on the Internet.
 
-DNS: Domain Name Services
+DNS: Domain Name System
 
+
+-: Route 53: Scalable DNS and Domain Name Registration
+Route 53 is a cloud domain name system (DNS) service that 
+has servers distributed around the globe used to translates 
+human-readable names like www.google.com into the 
+numeric IP addresses like 74.125.21.147.
+
+Features
+scales automatically to manage spikes in DNS queries
+allows you to register a domain name (or manage an existing)
+routes internet traffic to the resources for your domain
+checks the health of your resources
+
+Tips
+Route 53 is found under the Networking & Content Delivery 
+section on the AWS Management Console.
+Route 53 allows you to route users based on the user’s 
+geographic location.
+
+Identify one service not provided by Route 53.
+Website hosting
+
+but it does the ffg:
+domain name registrtion
+internet traffic routing
+health checking for resources
+
+Route 53 does not provide website hosting as it is only an 
+authoritative DNS service.
 
 
 
