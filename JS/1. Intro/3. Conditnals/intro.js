1019880bd4931bfb9ea60b34bb94ff1977bff259
HUGB <arababs2015@gmail.com>
2022-05-21 22:21:33 +0100

    L 2 L3  6

--- JS/1. Intro/3. Conditnals/intro.js
+++ JS/1. Intro/3. Conditnals/intro.js
@@ -28,6 +28,64 @@ if (/* this expression is true ) {
           // run this code
 }
 
+The value inside the if statement is always converted to true 
+or false. Depending on the value, the code inside the if 
+statement is run or the code inside the else statement is run, 
+but not both. The code inside the if and else statements are 
+surrounded by curly braces {...} to separate the conditions 
+and indicate which code should be run.
 
+Curly braces are not necessary if you have only one line of 
+code to execute following an if or else statement. They are 
+necessary if you have more than one line of code to execute.
+
+Problems Aren't Always Simple
+With most problem there's more things to consider before you 
+can actually solve the problem.
+
+Example: Deciding what to do this weekend
+Julia's plan is to hang out with her friend Colt at the park.
+
+For Julia's plans to work:
+Colt must be available
+The weather must be nice
+
+In JavaScript we can represent complex problems by combining 
+logical expressions with special operators called logical operators.
+
+Logical operators
+Logical operators can be used in conjunction with boolean values 
+(true and false) to create complex logical expressions.
+
+By combining two boolean values together with a logical operator, 
+you create a logical expression that returns another boolean value.
+
+||	Logical OR	value1 || value2	Returns true if either 
+                                        value1 or value2 (or even both!) evaluates 
+                                        to true
+ 
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+const balance = 320;
+const checkBal = true;
+const isActive = true;
+
+// your code goes here
+
+if (checkBal) {
+          if(isActive && balance > 0) {
+                    console.log("Your bal is $" + balance.toFixed(2)) 
+          } else if (!isActive) {
+                    console.log("Your account is no longer active")
+          } else if (isActive && balance ===  0) {
+                    console.log("Your account is empty")
+          } else {
+                    console.log("Your bal is negative. Pls contact bank")
+          }
+
+}else {
+          console.log("Thank you. Have a nice day!")
+}
 
 */ 
+
+
