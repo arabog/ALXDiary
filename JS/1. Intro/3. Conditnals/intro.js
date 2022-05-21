19654832c026507d20b3a75e5f61ac9abc5f100b
HUGB <arababs2015@gmail.com>
2022-05-21 20:30:02 +0100

    L 2 L3  1

---
+++ JS/1. Intro/3. Conditnals/intro.js
@@ -0,0 +1,18 @@
+/*
+We Write Code to Solve Problems
+When you write code, you break down problems into steps that are 
+executed by a computer. These steps are known as an algorithm.
+
+Example: Purchase Algorithm
+
+Think about the steps you need to take to decide whether to 
+purchase an item.
+
+Do I want the item?
+Do I have enough money to purchase the item?
+If the answer to both questions is yes, you will purchase the item. 
+The process you went through to solve that problem, and the steps 
+you took in order to do so, is at the heart of writing code.
+
+
+*/ 
