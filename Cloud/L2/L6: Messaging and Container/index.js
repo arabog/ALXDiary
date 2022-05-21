ca89dc47fce3bd5947b693a63997481ae23aedc7
HUGB <arababs2015@gmail.com>
2022-05-21 08:55:20 +0100

    L2 6   3

--- Cloud/L2/L6: Messaging and Container/index.js
+++ Cloud/L2/L6: Messaging and Container/index.js
@@ -14,5 +14,36 @@ applications and devices. One system can send a message
 to another system.
 
 
+Simple Notification Service
+Amazon Simple Notification Service (or SNS) is a cloud service 
+that allows you to send notifications to the users of your applications. 
+SNS allows you to decouple the notification logic from being 
+embedded in your applications and allows notifications to be 
+published to a large number of subscribers(wc can be a person 
+or anoda AWS service).
+
+Features
+SNS uses a publish/subscribe model.
+SNS can publish messages to Amazon SQS queues, AWS Lambda 
+functions, and HTTP/S webhooks.
+
+Tips
+SNS is found under the Application Integration section on the 
+AWS Management Console.
+SNS Topic names are limited to 256 characters.
+A notification can contain only one message.
+
+Q: Which ways can notifications be sent to users via SNS? Pick three.
+Notifications can be sent to end users using mobile push, text 
+messages, and email.
+
+
 
 */
+
+https://stackoverflow.com/questions/28429493/how-to-connect-mysql-workbench-to-amazon-rds
+
+
+SNS allows you to decouple the notification logic from being 
+embedded in your applications?
+mobile push?
