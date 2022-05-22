ab6ba00540d685fa896579efd14b6b3fb821783c
HUGB <arababs2015@gmail.com>
2022-05-22 15:04:10 +0100

    L2 L7  7

--- Cloud/L2/L&: AWS Mgt/info.js
+++ Cloud/L2/L&: AWS Mgt/info.js
@@ -87,6 +87,20 @@ up) resources in a reproducible way.
 https://en.wikipedia.org/wiki/Infrastructure_as_code
 
 
+-: Cloud Formation
+AWS Cloud Formation allows you to model your entire infrastructure 
+in a text file template allowing you to provision AWS resources based 
+on the scripts you write.
+
+Tips
+Cloud Formation is found under the Management & Governance 
+section on the AWS Management Console.
+Cloud Formation templates are written using JSON or YAML.
+You can still individually manage AWS resources that are part of 
+a CloudFormation stack.
+
+
+
 
 
 
