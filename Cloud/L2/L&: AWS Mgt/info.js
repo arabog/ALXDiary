ef66e729d661bcb6d9ed490f25740fb4b1e7f445
HUGB <arababs2015@gmail.com>
2022-05-22 15:00:38 +0100

    L2 L7 6

--- Cloud/L2/L&: AWS Mgt/info.js
+++ Cloud/L2/L&: AWS Mgt/info.js
@@ -76,6 +76,16 @@ Track metrics
 Cloud Watch can collect and track metrics, collect and monitor 
 log files, and create triggers to run your AWS resources.
 
+-: Infrastructure as Code
+Infrastructure as Code allows you to describe and provision all 
+the infrastructure resources in your cloud environment. You can 
+stand up servers, databases, runtime parameters, resources, etc. 
+based on scripts that you write. Infrastructure as Code is a 
+time-saving feature because it allows you to provision (or stand 
+up) resources in a reproducible way.
+
+https://en.wikipedia.org/wiki/Infrastructure_as_code
+
 
 
 
