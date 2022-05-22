c2e351d529e8a6d792214c11bb6c8a55a045ee66
HUGB <arababs2015@gmail.com>
2022-05-22 13:14:33 +0100

    L2 L7   3

--- Cloud/L2/L&: AWS Mgt/info.js
+++ Cloud/L2/L&: AWS Mgt/info.js
@@ -1,7 +1,7 @@
 /*
 Why do we need logging and auditing in the cloud?
 
-Logging In The Cloud
+-: Logging In The Cloud
 Logging provides visibility into your cloud resources and applications. 
 For applications that run in the cloud, you will need access to logging 
 and auditing services to help you proactively monitor your resources 
@@ -13,6 +13,35 @@ What is the current load on the server?
 What is the root cause of an application error that a user is seeing?
 What is the path that leads to this error?
 
+-: Cloud Trail
+Cloud Trail allows you to audit (or review) everything that occurs in 
+your AWS account. Cloud Trail does this by recording all the AWS API 
+calls occurring in your account and delivering a log file to you.
+
+Features
+CloudTrail provides event history of your AWS account activity, including:
+who has logged in
+services that were accessed
+actions performed
+parameters for the actions
+responses returned
+This includes actions taken through the AWS Management Console, 
+AWS Software Development Kits(SDKs), command line tools, and 
+other AWS services.
+
+Tips
+Cloud Trail is found under the Management & Governance section 
+on the AWS Management Console.
+CloudTrail shows results for the last 90 days.
+You can create up to five trails in an AWS region.
+
+Q: Identify features provided by Cloud Trail. Pick three.
+Set up alerts and alarms for certain activities
+Log responses from AWS services
+Track calls made using the SDK
+
+Cloud Trail allows you to audit (or review) everything that occurs 
+in your AWS account.
 
 
 
