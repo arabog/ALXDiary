f502143e0fa9062abd9d117461ab9e53b2d24fc7
HUGB <arababs2015@gmail.com>
2022-05-22 13:42:58 +0100

    L2 L7   4

--- Cloud/L2/L&: AWS Mgt/info.js
+++ Cloud/L2/L&: AWS Mgt/info.js
@@ -43,6 +43,15 @@ Track calls made using the SDK
 Cloud Trail allows you to audit (or review) everything that occurs 
 in your AWS account.
 
+-: Create a Trail
+AWS CloudTrail is a service that captures every event occurred in 
+your account, in the form of logs, for review and analysis. These 
+logs can be stored in Amazon S3 buckets or delivered to Amazon 
+CloudWatch logs, which helps you to set alarms and take 
+appropriate actions.
+
+
+
 
 
 */ 
