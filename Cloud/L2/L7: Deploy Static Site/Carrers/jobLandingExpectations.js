0085d16387e78be5321dc1ab0ee2f25138978655
HUGB <arababs2015@gmail.com>
2022-05-23 10:45:08 +0100

    L2 L7    4

---
+++ Cloud/L2/L7: Deploy Static Site/Carrers/jobLandingExpectations.js
@@ -0,0 +1,33 @@
+/*
+The first Career webinar is designed to walk you through all 
+that you can do to search for a job that aligns with your 
+technical Nanodegree
+
+Throughout this webinar, you will get to know more about 
+the types of jobs and job searching techniques in addition 
+to supporting you to leverage your job searching skills!
+
+Agenda of the Webinar:
+What is a job search?
+Types of jobs
+Job search journey (traditional jobs)
+Maximizing your chances
+Next steps
+
+
+https://www.16personalities.com/free-personality-test
+
+https://kevan.org/johari
+
+https://www.scienceofpeople.com/whats-intelligence-type/
+
+https://www.mint-hr.com/mumford.html
+
+https://www.indeed.com/career-advice/career-development/how-to-write-smart-goals
+
+
+
+
+
+
+*/ 
