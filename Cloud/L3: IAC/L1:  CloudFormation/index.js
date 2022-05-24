f09691244bb4d098a01e13f27a5a6523535ca385
HUGB <arababs2015@gmail.com>
2022-05-24 10:20:47 +0100

    L3 L1  2

--- Cloud/L3: IAC/L1:  CloudFormation/index.js
+++ Cloud/L3: IAC/L1:  CloudFormation/index.js
@@ -1,5 +1,5 @@
 /*
-What is (Cloud) Infrastructure?
+-: What is (Cloud) Infrastructure?
 Cloud services are broadly categorized as Software as a Service (SaaS), 
 Platform as a Service (PaaS), or Infrastructure as a Service (IaaS). Cloud 
 is a collection of geographically distributed data centers that are logically 
@@ -9,6 +9,41 @@ The IaaS allows a user to provision Virtual Machines (VMs), set up networks
 (VPC), create subnets, and associate necessary security features. Further, 
 VMs can be attached to storage volumes, different networks, or servers. 
 All the resources mentioned above are referred to as Infrastructure.
+
+-: Lesson Objectives
+After completing this lesson, you will be able to...
+Describe Infrastructure as a Code (IaaC) as one of the best practices 
+used in the DevOps model.
+
+Configure basic settings to start using AWS services as an Identity and 
+Access Management (IAM) user.
+
+Explain the fundamentals of Cloud Formation.
+
+Contrast the manual vs. automated (script-based) provisioning of 
+Elastic Compute Cloud (EC2) instances in a Virtual Private Cloud (VPC).
+
+Utilize the AWS command-line tool - CLI for necessary activities, such 
+as configuring a VPC, or creating an IAM user.
+
+Course Github Repository
+The link to the Github repository is - 
+https://github.com/udacity/nd9991-c2-Infrastructure-as-Code-v1 
+that contains the following:
+
+Supporting material used in this course, inside supporting_material folder. 
+It contains the files used in the demonstration videos in Lesson 1 to Lesson 5.
+Project starter code, inside project starter folder.
+
+-: What is DevOps
+DevOps = Development + Operatns
+
+Operatnal is d part of IT dt mnages, monitors, configures, and deploys software
+
+
+
+
+
 
 
 
