953dad3a86874d4682cf61fca81f24758332c6c6
HUGB <arababs2015@gmail.com>
2022-05-24 21:29:14 +0100

    JS Loops L4   15

--- JS/1. Intro/4. Loops/intro.js
+++ JS/1. Intro/4. Loops/intro.js
@@ -268,7 +268,27 @@ for (let i = 1; i <= 12; i++) {
 
 console.log(solution);
 
+-: Find my Seat
+Theater seats often display a row and seat number to help 
+theatergoers find their seats. If there are 26 rows (0 to 25) 
+and 100 seats (0 to 99) in each row, write a nested for loop 
+to print out all of the different seat combinations in the theater.
 
+Example output for row-seat information: output each row and seat number on a separate line
+
+0-0
+0-1
+0-2
+...
+25-97
+25-98
+25-99
+
+for(i = 0; i <= 25; i++) {
+          for(j = 0; j <= 99; j++) {
+                    console.log(`row:  ${i}: seat:  ${j}`);
+          }
+}
 
 
 
