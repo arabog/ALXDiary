496481ef6897634c7a926418d0cc522c49c60c0d
HUGB <arababs2015@gmail.com>
2022-05-24 10:08:59 +0100

    L3 L1  1

---
+++ Cloud/L3: IAC/L1:  CloudFormation/index.js
@@ -0,0 +1,16 @@
+/*
+What is (Cloud) Infrastructure?
+Cloud services are broadly categorized as Software as a Service (SaaS), 
+Platform as a Service (PaaS), or Infrastructure as a Service (IaaS). Cloud 
+is a collection of geographically distributed data centers that are logically 
+grouped into regions and availability-zones.
+
+The IaaS allows a user to provision Virtual Machines (VMs), set up networks 
+(VPC), create subnets, and associate necessary security features. Further, 
+VMs can be attached to storage volumes, different networks, or servers. 
+All the resources mentioned above are referred to as Infrastructure.
+
+
+
+
+*/ 
