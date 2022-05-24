2d3ac87cdeb65eaa367cc88c23f8279a9b5f1aa4
HUGB <arababs2015@gmail.com>
2022-05-24 10:42:14 +0100

    L3 L1  4

--- Cloud/L3: IAC/L1:  CloudFormation/index.js
+++ Cloud/L3: IAC/L1:  CloudFormation/index.js
@@ -46,11 +46,47 @@ Unpredictable deployments
 Mismatched environments (development doesn’t match production)
 Configuration Drift
 
+-: What are the benefits of Cloud DevOps?
+DevOps best practices and tools
+One of the benefits of using DevOps is that it allows predictable deployments 
+using automated scripts. In the DevOps model, development and operations 
+teams are merged into a single team. These DevOps teams use a few tools 
+and best practices that deploy and manage configuration changes to servers
+
+Difference between DevOps and Software Configuration Management
+https://softwareengineering.stackexchange.com/questions/130850/difference-between-devops-and-software-configuration-management
 
 
+The most important practices are:
+Continuous Integration / Continuous Delivery (CI/CD) - new features are 
+automatically deployed with all the required dependencies.
 
+Infrastructure as Code (IaaC) - configuration and management of cloud 
+infrastructure using re-usable scripts.
 
+Other prevalent practices are:
+Microservices
+Monitoring and Logging
+Communication and Collaboration
 
+Glossary
+Continuous Integration Continuous Deployment (CI/CD): Tracks 
+the development workflow from testing through production. 
+Continuous integration is the process flow of testing any change 
+made to your development flow, while continuous deployment tracks 
+those changes through to staging and production systems.
+
+Continuous integration vs. delivery vs. deployment
+https://www.atlassian.com/continuous-delivery/principles/continuous-integration-vs-delivery-vs-deployment
+
+Infrastructure as code (IaC): Provision and manages the cloud-infrastructure 
+by using scripts. These scripts can be written in YAML or JSON format. 
+These scripts ensure that the same architecture can be re-built multiple 
+numbers of times. These scripts are particularly useful in enterprise 
+applications and different environments - dev, prod, or test.
+
+Infrastructure as code
+https://en.wikipedia.org/wiki/Infrastructure_as_code
 
 
 
