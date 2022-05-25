2f204f5123c155568b009278fb56ee18b0b86085
HUGB <arababs2015@gmail.com>
2022-05-25 11:09:01 +0100

    ALX L3 L2  2

--- Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
+++ Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
@@ -6,6 +6,22 @@ cloud infrastructure. DevOps engineers start with a visual
 representation of the required cloud infrastructure before they 
 turn it into code. 
 
+A VPC is simply a network segment or container that is isolated
+from d oda networks It can talk to other netwrks but nt d internet
+unless there is a specific routing for it
+
+Make use of www.lucidchart.com to draw the diagram.
+https://www.lucidchart.com/pages/
+
+Learn about architecting at scale
+https://lethain.com/introduction-to-architecting-systems-for-scale/
+
+
+https://github.com/udacity/nd9991-c2-Infrastructure-as-Code-v1/tree/master/supporting_material
+
+
+
+
 
 
 
