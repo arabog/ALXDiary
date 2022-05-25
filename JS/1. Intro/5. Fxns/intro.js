b80ad65a5dfe1d7bc5043a70bfd66fcb2ccd8be8
HUGB <arababs2015@gmail.com>
2022-05-25 16:48:53 +0100

    ALX JS L5 24

--- JS/1. Intro/5. Fxns/intro.js
+++ JS/1. Intro/5. Fxns/intro.js
@@ -1122,7 +1122,18 @@ const cry = function named() {
           returns "boohoo";
 }
 
--: 
+-: Quiz: Inline
+Call the emotions() function so that it prints the output you see below, 
+but instead of passing the laugh() function as an argument, pass an 
+inline function expression instead.
+
+emotions("happy", laugh(2)); // you can use your laugh function from 
+the previous quizzes
+
+Prints: "I am happy, haha!"
+
+
+
 
 */ 
 
