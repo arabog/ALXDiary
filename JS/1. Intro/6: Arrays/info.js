e566f268ba3e4ed847c4e0103ad1ca8fdd8346e9
HUGB <arababs2015@gmail.com>
2022-05-26 16:05:18 +0100

    L6 Arr    13

--- JS/1. Intro/6: Arrays/info.js
+++ JS/1. Intro/6: Arrays/info.js
@@ -127,5 +127,29 @@ rainbow.splice(2, 1,  "Yellow", "Green");
 rainbow.splice(-1, 0,  "Purple");
 console.log(rainbow);
 
+Q: Consider the following array, removeFirstElement:
+const removeFirstElement = ["a", "b", "c"];
+
+Let's say that you want to modify (i.e., mutate) removeFirstElement 
+by removing the first element within it. Which method(s) could you use?
+shift() and splice()
+
+shift() will remove the first element from an array.
+splice() can be used if you specify the index of the first element, and 
+indicate that you want to delete 1 element.
+
+Keep in mind that the slice() method allows you to create a copy 
+of the array between two indices. Though you could just exclude 
+the index of the first element, this approach does not directly 
+modify a given array.
+
+Q: What method would be best for changing this array into a string?
+const turnMeIntoAString = ["U", "d", "a", "c", "i", "t", "y"];
+join()
+
+You can combine the elements in an array to form a string using the join() method.
+
+
+
 */ 
 
