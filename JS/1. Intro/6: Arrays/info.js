d311a08f873c3b4d52dcb45b4820fa90b30fdfff
HUGB <arababs2015@gmail.com>
2022-05-26 16:47:26 +0100

    L6 Arr    18

--- JS/1. Intro/6: Arrays/info.js
+++ JS/1. Intro/6: Arrays/info.js
@@ -289,7 +289,70 @@ The outer loop will cycle through each row and then in the row
 that's currently being iterated over, the inner loop will iterate 
 over each cell.
 
+-: 2D Donut Arrays
+A box of donuts is like a two-dimensional grid with rows and columns
+You could use an array of arrays that has the name of each donut 
+associated with its position in the box.
 
+Here's an example:
+
+const donutBox = [
+          ["glazed", "chocolate glazed", "cinnamon"],
+          ["powdered", "sprinkled", "glazed cruller"],
+          ["chocolate cruller", "Boston creme", "creme de leche"]
+];
+
+If you wanted to loop over the donut box and display each donut 
+(along with its position in the box!) you would start with writing 
+a for loop to loop over each row of the box of donuts:
+
+const donutBox = [
+          ["glazed", "chocolate glazed", "cinnamon"],
+          ["powdered", "sprinkled", "glazed cruller"],
+          ["chocolate cruller", "Boston creme", "creme de leche"]
+];
+
+// here, donutBox.length refers to the number of rows of donuts
+for (let row = 0; row < donutBox.length; row++) {
+          console.log(donutBox[row]);
+}
+
+Prints:
+["glazed", "chocolate glazed", "cinnamon"]
+
+["powdered", "sprinkled", "glazed cruller"]
+
+["chocolate cruller", "Boston creme", "creme de leche"]
+
+Since each row is an array of donuts, you next need to set up an 
+inner-loop to loop over each cell in the arrays.
+
+for (let row = 0; row < donutBox.length; row++) {
+          // here, donutBox[row].length refers to the length of the donut array currently being looped over
+          for (let column = 0; column < donutBox[row].length; column++) {
+                    console.log(donutBox[row][column]);
+          }
+}
+
+Prints:
+
+"glazed"
+
+"chocolate glazed"
+
+"cinnamon"
+
+"powdered"
+
+"sprinkled"
+
+"glazed cruller"
+
+"chocolate cruller"
+
+"Boston creme"
+
+"creme de leche"
 
 
 
