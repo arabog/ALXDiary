9fe0a76041798bc4bfc9a04890fd08721160d4da
HUGB <arababs2015@gmail.com>
2022-05-26 10:06:26 +0100

    L3 L2   18

--- Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
+++ Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
@@ -220,6 +220,7 @@ the servers (EC2 instances) according to the amount of incoming traffic. This
 behavior of the autoscaling group helps in two ways:
 
 The consumer pays for the only duration of the servers when they were active.
+
 The consumer doesn't have to worry about horizontal scaling of servers for a 
 sudden peak in incoming traffic.
 
@@ -232,6 +233,34 @@ in one of the availability zones.
 
 If there is a failure of that resource, the autoscaling group will shut it down in 
 that availability zone and start that same resource in the other availability zone.
+
+https://docs.aws.amazon.com/autoscaling/ec2/userguide/auto-scaling-groups.html
+
+-: Load Balancers
+A target grp is a collection of servers providing a common service
+
+Load Balancer
+A load balancer takes incoming traffic and distributes it to two or more 
+resources. For example, it can take inbound user requests to access your 
+website, and it can distribute the requests evenly among two or more servers.
+
+Without a load balancer, having public-facing servers in more than one AZ 
+would mean that users would have to use a different URL to reach each of 
+the AZs. This can be impractical compared to just a single URL.
+
+Good practice - Assume we have a set of web-servers in private subnet(s). 
+Then, we must have a Load Balancer that would access our web-servers. 
+These web-servers, in turn, would access the backend database.
+
+AWS - Elastic Load Balancing
+We recommend you to read about three types of load balancing offered 
+by AWS at different layers of networking protocol:
+
+https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/introduction.html
+
+https://docs.aws.amazon.com/elasticloadbalancing/latest/application/introduction.html
+
+https://docs.aws.amazon.com/elasticloadbalancing/latest/network/introduction.html
 
 
 
