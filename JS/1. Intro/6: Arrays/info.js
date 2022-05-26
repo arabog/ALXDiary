e85bc577a67064134905022b363e504b5bebf669
HUGB <arababs2015@gmail.com>
2022-05-26 15:47:19 +0100

    L6 Arr    11

--- JS/1. Intro/6: Arrays/info.js
+++ JS/1. Intro/6: Arrays/info.js
@@ -85,10 +85,34 @@ from the end of an array, splice() lets you specify the index location
 to add new elements, as well as the number of elements you'd like to 
 delete (if any).
 
+const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
 
+// removes "chocolate frosted" at index 1 and adds "chocolate cruller" and "creme de leche" starting at index 1
+donuts.splice(1, 1, "chocolate cruller", "creme de leche"); 
 
+Returns: ["chocolate frosted"]
+donuts array after calling the splice() method: ["glazed", "chocolate cruller", "creme de leche", "Boston creme", "glazed cruller"]
 
+Following is the syntax of **splice()** method: arrayName.splice(arg1, arg2, item1, ....., itemX); where,
 
+arg1 = Mandatory argument. Specifies the starting index position to 
+add/remove items. You can use a negative value to specify the position 
+from the end of the array e.g., -1 specifies the last element.
+
+arg2 = Optional argument. Specifies the count of elements to be removed. 
+If set to 0, no items will be removed.
+
+item1, ....., itemX are the items to be added at index position arg1
+splice() method returns the item(s) that were removed.
+
+We’ve decided to replace some of the donuts in the donuts array.
+
+const donuts = ["cookies", "cinnamon sugar", "creme de leche"];
+
+donuts.splice(-2, 0, "chocolate frosted", "glazed");
+What does the donuts array look like after the following changes?
+
+const donuts = ["cookies", "chocolate frosted", "glazed", "cinnamon sugar", "creme de leche"];
 
 */ 
 
