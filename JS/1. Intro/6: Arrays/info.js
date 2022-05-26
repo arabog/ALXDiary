1657a630f36c2201448650c4d971be5155b98954
HUGB <arababs2015@gmail.com>
2022-05-26 15:07:46 +0100

    L6 Arr    2

--- JS/1. Intro/6: Arrays/info.js
+++ JS/1. Intro/6: Arrays/info.js
@@ -1,6 +1,23 @@
 /*
 The numbering of the array order starts at 0 -- not at 1
 
+You can even store an array in an array to create a nested array!
+
+// creates a `arraysInArrays` array with three arrays
+const arraysInArrays = [
+          [1, 2, 3], 
+          ["Julia", "James"], 
+          [true, false, true, false]
+];
+
+-: Accessing Array Elements
+Elements and Indexes
+An element is an individual piece of data in an array.
+Each element in the array is numbered, starting with 0. This number is called the index.
+The index allows us to access the element's position in the array.
+
+
+
 
 
 */ 
