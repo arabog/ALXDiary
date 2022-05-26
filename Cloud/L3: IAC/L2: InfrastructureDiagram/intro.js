13b264a2eddbea3e2117eb481d384f5901902b34
HUGB <arababs2015@gmail.com>
2022-05-26 09:45:11 +0100

    L3 L2   17

--- Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
+++ Cloud/L3: IAC/L2: InfrastructureDiagram/intro.js
@@ -211,6 +211,31 @@ requests from resources in a private subnet.
 https://docs.aws.amazon.com/vpc/latest/userguide/vpc-nat-gateway.html
 
 
+-: Autoscaling groups
+Glossary
+Autoscaling group: It is a coherent group of Virtual Machines (EC2 instances) 
+that allows running the exact number of VMs that are required to meet the 
+demand/specification. The autoscaling group can automatically start or stop 
+the servers (EC2 instances) according to the amount of incoming traffic. This 
+behavior of the autoscaling group helps in two ways:
+
+The consumer pays for the only duration of the servers when they were active.
+The consumer doesn't have to worry about horizontal scaling of servers for a 
+sudden peak in incoming traffic.
+
+Best Practice
+It is recommended that an autoscaling group spans more than one availability 
+zone, for reliability.
+
+If we set the autoscaling group to run one resource, it will run that one resource 
+in one of the availability zones.
+
+If there is a failure of that resource, the autoscaling group will shut it down in 
+that availability zone and start that same resource in the other availability zone.
+
+
+
+
 */ 
 
 Is this statement correct IP addresses are in VPC but they are used in Subnets?
