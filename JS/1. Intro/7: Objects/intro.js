a140a5f8c47ee0dd4b2566b84162678f1ef630c0
HUGB <arababs2015@gmail.com>
2022-05-27 15:58:39 +0100

    ALX 3 JS  L7   2

--- JS/1. Intro/7: Objects/intro.js
+++ JS/1. Intro/7: Objects/intro.js
@@ -4,6 +4,60 @@ Objects are Powerful!
 Objects allow us to wrap up pieces of related data and 
 functionality into one single container.
 
+Using typeof
+typeof is handy operator that returns the name of the 
+data type that follows it.
+
+const umbrella = {
+          color: "pink",
+          isOpen: false,
+
+          open: function() {
+                    if(umbrella.isOpen === true) {
+                              return "The umbrella is already opened!";
+                    } else {
+                              umbrella.isOpen = true;
+
+                              return "Julia opens the umbrella!";
+                    }
+          }
+};
+
+Objects are JavaScript Object types that are assigned by 
+reference, not by value.
+
+const umbrella = {
+          color: "pink",
+          isOpen: false,
+
+          open: function() {
+                    if(umbrella.isOpen === true) {
+                              return "The umbrella is already opened!";
+                    } else {
+                              umbrella.isOpen = true;
+
+                              return "Julia opens the umbrella!";
+                    }
+          },
+
+          close: () => {
+                    if(umbrella.isOpen === true) {
+                              umbrella.isOpen = false;
+
+                              return "Julia closes the umbrella!";
+                    } else {
+                              return "The umbrella is already closed!";
+                    }
+          }
+
+
+};
+
+console.log(umbrella.open());
+console.log(umbrella.close());
+
+
+
 
 
 */ 
