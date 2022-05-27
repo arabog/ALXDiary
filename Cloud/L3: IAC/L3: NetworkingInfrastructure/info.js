bdac291f8212240364a356ef5fbf8ed9e0e65ddc
HUGB <arababs2015@gmail.com>
2022-05-27 12:59:24 +0100

    ALX L3 L3   4

--- Cloud/L3: IAC/L3: NetworkingInfrastructure/info.js
+++ Cloud/L3: IAC/L3: NetworkingInfrastructure/info.js
@@ -22,5 +22,63 @@ using the private IP address. Therefore, the elastic load balancer
 can directly distribute the ingress requests across multiple targets 
 VMs with the same VPC, even if the VMs are in a private subnet. 
 
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-template-resource-type-ref.html
+
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/resources-section-structure.html
+
+-: Outputs: 
+You should not output any sensitive information, such as passwords 
+or secrets. For each resource's output, you will have to provide the 
+following:
+
+Description (optional) - A string
+Value (required) - The property returned by the aws cloudformation 
+describe-stacks command.
+
+Export (optional) - The name of the resource output to be exported for 
+a cross-stack reference.
+
+Understanding the JSON (Parameters) file
+Why do we need a separate parameter file (JSON)?
+The separate parameter file (JSON) file helps us to avoid hard-coding 
+the parameters in the template (YAML) file.
+
+
+Any named parameters in the Parameters section of our CloudFormation 
+template will need to have a matching value in a separate, Parameter file, 
+which is in JSON format. The YAML format is not yet supported for the 
+parameter file. A sample JSON-formatted Parameter file is:
+
+[
+          {
+                    "ParameterKey": "EnvironmentName",
+                    "ParameterValue": "UdacityProject"
+          }
+]
+
+Having this additional file with actual parameter values allows you 
+to change data used by your CloudFormation template without the 
+risk of modifying the template directly and possibly introducing a 
+typo or some logical error.
+
+How to execute the Shell scripts?
+You can run either of the shell scripts (create.sh or update.sh) as:
+./<file_name> argument_1 argument_2 argument_3
+
+For example, you can run the following command:
+./create.sh ourdemoinfra ourinfra.yml ourinfra.json
+
+The command above is equivalent of running the following:
+aws cloudformation create-stack --stack-name ourdemoinfra --template-body file://ourinfra.yml    --parameters file://ourinfra.json  --region=us-east-1
+
+Troubleshoot
+While running the AWS commands using either create.sh or update.sh file, 
+if you face permission denied error, then you will have to grant the execute 
+permission to the owner (yourself) explicitly as:
+
+chmod +x update.sh 
+chmod +x create.sh 
+
+
 
 */ 
