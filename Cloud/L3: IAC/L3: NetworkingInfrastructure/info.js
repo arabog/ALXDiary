108802349edd7eb5cdee31f0dbb379d8c9281246
HUGB <arababs2015@gmail.com>
2022-05-27 10:48:56 +0100

    ALX L3 L3   2

--- Cloud/L3: IAC/L3: NetworkingInfrastructure/info.js
+++ Cloud/L3: IAC/L3: NetworkingInfrastructure/info.js
@@ -9,6 +9,18 @@ Build the following resources -
           Route table
 Export the stack output
 
+-: Workflow and Helpers
+The NAT gateway serves as an intermediary to take a private resource's 
+outbound request, connect to the Internet gateway, and then relay the 
+response back to the private resource without exposing that private 
+resource's IP address to the public. 
+
+We will deploy the (publicly visible) load balancer in the same 
+VPC having both public and private subnets. Note that all 
+resources within a VPC can connect to each other directly 
+using the private IP address. Therefore, the elastic load balancer 
+can directly distribute the ingress requests across multiple targets 
+VMs with the same VPC, even if the VMs are in a private subnet. 
 
 
 */ 
