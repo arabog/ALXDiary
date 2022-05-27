99a88f7540fc9f0babb6fe0e8d0183bd0d3a5fb4
HUGB <arababs2015@gmail.com>
2022-05-27 10:00:58 +0100

    ALX L3 L3   1

---
+++ Cloud/L3: IAC/L3: NetworkingInfrastructure/info.js
@@ -0,0 +1,14 @@
+/*
+-: Lesson Objectives
+Upon completion of this lesson, you will be able to:
+
+Describe the syntax of the CloudFormation code that builds the infrastructure
+Build the following resources -
+          Virtual Private Cloud and subnets
+          Internet gateway and NAT gateway
+          Route table
+Export the stack output
+
+
+
+*/ 
