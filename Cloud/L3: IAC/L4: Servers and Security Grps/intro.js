6abd9e35aa7039a6bfdf1415c4a13c57856f8d02
HUGB <arababs2015@gmail.com>
2022-05-29 17:16:12 +0100

    server and security grp   3

--- Cloud/L3: IAC/L4: Servers and Security Grps/intro.js
+++ Cloud/L3: IAC/L4: Servers and Security Grps/intro.js
@@ -26,9 +26,43 @@ the load balancer.
 https://github.com/udacity/nd9991-c2-Infrastructure-as-Code-v1/tree/master/supporting_material
 
 
+-: Understanding Security Groups
+Recall that a security group defines the firewall rules, 
+such as the protocol to open to network traffic and the 
+set of valid IP addresses.
 
+We will create two Security groups:
+          WebServerSecGroup for the webserver
+LBSecGroup for a load balancer
 
+Security Group Syntax
+The following is the syntax required to create a SecurityGroup:
+          Type: AWS::EC2::SecurityGroup
+          Properties: 
+          GroupDescription: String
+          GroupName: String
+          SecurityGroupEgress: 
+                    - Egress
+          SecurityGroupIngress: 
+                    - Ingress
+          Tags: 
+                    - Tag
+          VpcId: String
 
+In the SecurityGroup syntax shown above:
+The only required (mandatory) property is GroupDescription. 
+It is a String value up to 255 characters without quotes.
+
+The GroupName is similar to GroupDescription, but it's 
+not a required property.
+
+The SecurityGroupEgress and SecurityGroupIngress property 
+rules are the most critical as it defines where the traffic will 
+go. The former defines outbound traffic, whereas the latter 
+defines the inbound traffic.
+
+The VpcId denotes the VPC ID in which you are creating 
+the Security Group.
 
 
 
