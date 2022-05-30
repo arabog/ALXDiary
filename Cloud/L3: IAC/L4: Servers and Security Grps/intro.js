15901c547fa78246f3e968cbb4e99f2e63bdd6c5
HUGB <arababs2015@gmail.com>
2022-05-30 11:07:43 +0100

    server and security grp  10

--- Cloud/L3: IAC/L4: Servers and Security Grps/intro.js
+++ Cloud/L3: IAC/L4: Servers and Security Grps/intro.js
@@ -800,7 +800,96 @@ aws cloudformation create-stack --stack-name ourdemoservers --template-body file
           ToPort: 8080
           CidrIp: 0.0.0.0/0
 
+https://aws.amazon.com/premiumsupport/knowledge-center/public-load-balancer-private-ec2/
 
+
+How do I connect a public-facing load balancer to EC2 instances that have private IP addresses?
+https://www.youtube.com/watch?v=9Ut0cEWV9NQ
+
+
+-: Connect to private servers via a Jumpbox
+SSH - Secure SHell
+
+login
+ssh ec2-user@<private IPv4 address> -i filename.pem
+
+copy
+scp -i filename.pem private-server-devops-key.pem ec2-user@<private IPv4 address>:/home/ec2-user/private-server-devops-key.pem
+
+verify
+ls
+
+chmod 400 private-server-devops-key.pem
+
+What is a Jumpbox?
+These are the EC2 instances in the public subnet with 
+required access to the private subnets' servers. Generally, 
+we would not want our private servers to be discoverable 
+by anyone outside the VPC. However, we want to access 
+those private servers from the Internet. It is made possible 
+by using a Jumpbox (also called as Bastion Host). It is 
+important to note that the security group attached to the 
+private servers must allow the IP address of the public 
+Jumpbox to connect to it.
+
+Create a Jumpbox
+In the demo video above, the instructor shows creating a 
+new key-pair, jumpbox-key.pem. However, you can use an 
+existing key-pair if available. Remember that key-pairs are 
+specific to an AWS region. Also, the VPC you have to choose 
+while launching the Jumpbox must be the same one in which 
+you have been creating Cloudformation stacks.
+
+https://classroom.udacity.com/nanodegrees/nd9991-alg-t2/parts/cd0648/modules/71d62f39-2467-4131-b9fd-fd372e818257/lessons/c7659a71-f18f-4674-96e8-7d7fc865e6a7/concepts/98f211d6-9152-4928-9d19-057dc727faf4
+
+Connect to the Jumpbox
+Since the Jumpbox is a Linux machine, therefore you will have 
+to use SSH protocol to connect to it. Remember that the flow of 
+connection will be
+
+Your local computer (Mac/Windows/Linux) → 
+Jumpbox(CentOS Linux) → 
+Private servers (Ubuntu Linux).
+
+Test the Jumpbox
+Recall that the instructor has the following two different 
+login keys: 1). jumpbox-key.pem for the Jumpbox and 2). 
+private-server-devops-key.pem for the private servers. 
+Let's test if you are able to connect to the private servers 
+via a Jumpbox:
+
+Copy the public IP address of the Jumpbox, say 3.17.80.159
+
+Copy and paste the private servers' login key file from your 
+local computer to the Jumpbox. Run the following command 
+from your local terminal (replace the file names and the IP 
+  address as applicable to you):
+
+  scp -i jumpbox-key.pem private-server-devops-key.pem ec2-user@3.17.80.159:/home/ec2-user/private-server-devops-key.pem
+
+  SSH login to the Jumpbox:
+ssh ec2-user@3.17.80.159 -i jumpbox-key.pem
+
+Copy the private IP address of any private server, say 10.0.2.74.
+
+Once you are logged into the Jumpbox, confirm if you have 
+the private-server-devops-key.pem key available in the home 
+directory, and then change the access-mode of the key file. 
+Later, try to SSH login to the private server:
+
+ls
+# you must see the private-server-devops-key.pem file
+chmod 400 private-server-devops-key.pem
+ssh -i "private-server-devops-key.pem" ubuntu@10.0.2.74
+
+Recall that the default user name for a Linux system is 
+ec2-user and for an Ubuntu system is ubuntu.
+
+Lastly, check the status of the running web server in the private instance
+
+Similarly, you can SSH login to the second private server 
+from the Jumpbox. You have to replace the private IP 
+address of the second server in the commands above.
 
 
 */ 
