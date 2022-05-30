e92449d294b6004ed9ab3905356cd68fc07a7282
HUGB <arababs2015@gmail.com>
2022-05-30 14:52:04 +0100

    Object Oriented Prog L     2

--- JS/3. Obj-Oriented-Programming/L1:  Object in dept/intro.js
+++ JS/3. Obj-Oriented-Programming/L1:  Object in dept/intro.js
@@ -111,6 +111,232 @@ https://mathiasbynens.be/notes/javascript-identifiers-es6
 
 
 -: create and modify ppties
+To create a new, blank (i.e., “empty”) object, you can use 
+object literal notation, or the Object() constructor function.
+
+// Using literal notation:
+const myObject = {};
+
+// Using the Object() constructor function:
+const myObject = new Object();
+
+Adding Properties
+const printer = {};
+
+printer.on = true;
+printer.mode = 'black and white';
+
+printer['remainingSheets'] = 168;
+
+Likewise, we can add a method to the printer object in a 
+similar manner. This time, the value of the property is an 
+anonymous (i.e., unnamed) function:
+
+printer.print = function () {
+          console.log('The printer is printing!');
+};
+
+Great! The complete printer object now looks like the following:
+
+{
+          on: true,
+          mode: 'black and white',
+          remainingSheets: 168,
+
+          print: function () {
+                    console.log('The printer is printing!');
+          }
+}
+
+Removing Properties
+Recall that since objects are mutable, not only can we 
+modify existing properties (or even add new ones) -- 
+we can also delete properties from objects
+
+delete printer.mode;
+//  true
+
+this will delete d mode key (along with its value, black and white) 
+
+Passing Arguments
+Passing a Primitive
+In JavaScript, a primitive (e.g., a string, number, boolean, etc.) 
+is immutable. In other words, any changes made to an argument 
+inside a function effectively creates a copy local to that function, 
+and does not affect the primitive outside of that function.
+
+Check out the following example:
+
+function changeToEight(n) {
+          n = 8; // whatever n was, it is now 8... but only in this function!
+}
+
+let n = 7;
+
+changeToEight(n);
+
+console.log(n);
+// 7
+changeToEight() takes in a single argument, n, and changes 
+it to 8. However, this change only exists inside the function 
+itself. We then pass the global variable n (which is assigned 
+the value 7) into the function. After invoking it, n is still equal to 7.
+
+Passing an Object
+On the other hand, objects in JavaScript are mutable. 
+If you pass an object into a function, Javascript passes 
+a reference to that object. Let's see what happens if we 
+pass an object into a function and then modify a property:
+
+let originalObject = {
+          favoriteColor: 'red'
+};
+
+function setToBlue(object) {
+          object.favoriteColor = 'blue';
+}
+
+setToBlue(originalObject);
+
+originalObject.favoriteColor;
+// 'blue'
+
+In the above example, originalObject contains a single property, 
+favoriteColor, which has a value of 'red'. We pass originalObject
+into the setToBlue() function and invoke it. After accessing 
+originalObject's favoriteColor property, we see that the value is 
+now 'blue'!
+
+How did this happen? Well, since objects in JavaScript are 
+passed by reference, if we make changes to that reference, 
+we're actually directly modifying the original object itself!
+
+What's more: the same rule applies when re-assigning an 
+object to a new variable, and then changing that copy. 
+Again, since objects are passed by reference, the original 
+object is changed as well. 
+
+Consider this iceCreamOriginal object, which shows the 
+amount of ice cream cones each instructor has eaten:
+
+const iceCreamOriginal = {
+          Andrew: 3,
+          Richard: 15
+};
+
+Let's go ahead and make assign a new variable to iceCreamOriginal. 
+We'll then check the value of its Richard property:
+
+const iceCreamCopy = iceCreamOriginal;
+
+iceCreamCopy.Richard;
+// 15
+As expected, the expression iceCreamCopy.Richard; returns 
+15 (i.e., it is the same value as the Richard property in 
+iceCreamOriginal). Now, let's change the value in the copy, 
+then check the results:
+
+iceCreamCopy.Richard = 99;
+
+iceCreamCopy.Richard;
+// 99
+
+iceCreamOriginal.Richard;
+// 99
+Since objects are passed by reference, making changes to the 
+copy (iceCreamCopy) has a direct effect on the original object 
+(iceCreamOriginal) as well. In both objects, the value of the 
+Richard property is now 99.
+
+Comparing an Object with Another Object
+On the topic of references, let's see what happens when 
+we compare one object with another object. The 
+following objects, parrot and pigeon, have the same 
+methods and properties:
+
+const parrot = {
+          group: 'bird',
+          feathers: true,
+
+          chirp: function () {
+                    console.log('Chirp chirp!');
+          }
+};
+
+const pigeon = {
+          group: 'bird',
+          feathers: true,
+
+          chirp: function () {
+                    console.log('Chirp chirp!');
+          }
+};
+
+Naturally, one might expect the parrot object and pigeon 
+object to be equal. After all, both objects look exactly the 
+same! Let's compare parrot and pigeon to find out:
+
+parrot === pigeon;
+
+// false
+What's going on here? As it turns out, the expression will 
+only return true when comparing two references to exactly 
+the same object. Using what we now know about passing 
+objects, let's confirm this. To start off, let's create a new 
+variable, myBird, and assign it to one of the objects above:
+
+const myBird = parrot;
+As we've just learned, myBird not only refers to the same 
+object as parrot -- they are the same object! If we make 
+any updates to myBird's properties, parrot's properties 
+will be updated with exactly the same changes as well. 
+Now, the comparison will return true:
+
+myBird === parrot;
+
+// true
+So since pigeon is not the same object as myBird or parrot, 
+any comparisons between myBird and pigeon will return false:
+
+myBird === pigeon;
+// false
+
+Q: Consider the following:
+
+let string = 'orange';
+
+function changeToApple(string) {
+          string = 'apple';
+}
+
+changeToApple(string);
+
+console.log(string);
+// ???
+What is logged to the console?
+
+
+Within changeToApple(), string is assigned to 'apple'. However, 
+this change is only relevant within the function; outside, the 
+value of string remains 'orange'.
+
+Summary
+Objects are commonly created with literal notation, and 
+can include properties that point to functions called methods. 
+Methods are accessed the same way as other properties of 
+objects, and can be invoked the same way as regular 
+functions, except they automatically have access to the 
+other properties of their parent object.
+
+By default, objects are mutable (with a few exceptions), so 
+data within them can be altered. New properties can be 
+added, and existing properties can be modified by simply 
+specifying the property name and assigning (or re-assigning) 
+a value. Additionally, properties and methods of an object 
+can be deleted as well with the delete operator, which directly 
+mutates the object.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
 
 
 
@@ -133,3 +359,5 @@ https://mathiasbynens.be/notes/javascript-identifiers-es6
 
 -: Summary
 */ 
+
+Reference and assignment / copy in primitive and non-primitive values?
