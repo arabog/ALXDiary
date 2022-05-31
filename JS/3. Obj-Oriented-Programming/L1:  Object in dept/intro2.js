4aeb2af3af6b744f94a5972bfd3944e201f45c28
HUGB <arababs2015@gmail.com>
2022-05-31 20:48:02 +0100

    js  class 4 done

--- JS/3. Obj-Oriented-Programming/L1:  Object in dept/intro2.js
+++ JS/3. Obj-Oriented-Programming/L1:  Object in dept/intro2.js
@@ -154,8 +154,116 @@ Only consume() and iceCreamEaten are declared globally, so only
 these two identifiers are accessible as properties on window.
 
 
+Avoid Globals
+Counterintuitively, though, global variables and functions are not 
+ideal. There are actually a number of reasons why, but the two we'll 
+look at are:
+Tight coupling
+Name collisions
 
+Tight Coupling
+Tight coupling is a phrase that developers use to indicate code that is 
+too dependent on the details of each other. The word "coupling" means 
+the "pairing of two items together." In tight coupling, pieces of code 
+are joined together in a way where changing one unintentionally alters 
+the functioning of some other code:
 
+var instructor = 'Richard';
 
+function richardSaysHi() {
+          console.log(`${instructor} says 'hi!'`);
+}
+
+In the code above, note that the instructor variable is declared globally. 
+The richardSaysHi() function does not have a local variable that it uses 
+to store the instructor's name. Instead, it reaches out to the global variable 
+and uses that. If we refactored this code by changing the variable from 
+instructor to teacher, this would break the richardSaysHi() function (or 
+we'd have to update it there, too!). This is a (simple) example of 
+tightly-coupled code.
+
+Name Collisions
+A name collision occurs when two (or more) functions depend on a variable 
+with the same name. A major problem with this is that both functions will 
+try to update the variable and or set the variable, but these changes are 
+overridden by each other!
+
+Let's look at an example of name collision with this DOM manipulation code:
+
+let counter = 1;
+
+function addDivToHeader () {
+          const newDiv = document.createElement('div');
+          newDiv.textContent = 'div number ' + counter;
+
+          counter = counter + 1;
+
+          const headerSection = document.querySelector('header');
+          headerSection.appendChild(newDiv)
+}
+
+function addDivToFooter() {
+          const newDiv = document.createElement('div');
+          newDiv.textContent = 'div number ' + counter;
+
+          counter = counter + 1;
+
+          const headerSection = document.querySelector('footer');
+          headerSection.appendChild(newDiv)
+}
+
+In this code, we have an addDivToHeader() function and a 
+addDivToFooter() function. Both of these functions create a 
+<div> element and increment a counter variable.
+
+This code looks fine, but if you try running this code and adding 
+a few <div>s to the <header> and <footer> elements, you'll find 
+that the numbering will get off! Both addDivToHeader() and 
+addDivToFooter() expect a global counter variable to be accessible 
+to them -- not change out from under them!
+
+Since both functions increment the counter variable, if the code 
+alternates between calling addDivToHeader() and addDivToFooter(), 
+then their respective <div>s will not have numerically ascending 
+numbers. For example, if we had the following calls:
+
+addDivToHeader();
+addDivToHeader();
+
+addDivToFooter();
+
+addDivToHeader();
+
+The developer probably wanted the <header> to have three <div> 
+elements with the numbers 1, 2, and 3 and the <footer> element to 
+have a single <div> with the number 1. However, what this code 
+will produce is a <header> element with three <div> but with the 
+numbers 1, 2, and 4 (not 3) and a <footer> element with the number 
+3...these are very different results. But it's happening because both 
+functions depend on the counter variable and both update it.
+
+So what should you do instead? You should write as few global 
+variables as possible. Write your variables inside of the functions 
+that need them, keeping them as close to where they are needed as 
+possible. Now, there are times when you'll need to write global 
+variables, but you should only write them as a last resort.
+
+Summary
+The window object is provided by the browser and is not part of 
+the JavaScript language or specification. Any global variable 
+declarations (i.e., those that use var) or global function declarations 
+are added as properties to this window object. Excessive use of global 
+variables is not a good practice, and can cause unexpected problems 
+to accurately-written code.
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window
+
+https://html.spec.whatwg.org/multipage/window-object.html#the-window-object
+
+http://wiki.c2.com/?GlobalVariablesAreBad
+
+https://en.wikipedia.org/wiki/Coupling_(computer_programming)
+
+https://en.wikipedia.org/wiki/Name_collision
 
 */ 
