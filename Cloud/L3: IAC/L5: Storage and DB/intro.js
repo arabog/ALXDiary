d11030a0e12d909b4951db11fc8f7ba7b7ea4ac3
HUGB <arababs2015@gmail.com>
2022-06-01 15:41:40 +0100

    L5 init

---
+++ Cloud/L3: IAC/L5: Storage and DB/intro.js
@@ -0,0 +1,22 @@
+/*
+-: Intro
+Q: It's perfectly fine to use S3 to store which of the following 
+response options?
+Images/Video files
+Configuration files
+Log files
+
+These files will be accessed by your application and won't need 
+to be on the app server itself, making it ideal for transitory 
+infrastructure.
+
+
+
+
+
+
+
+
+
+
+*/ 
