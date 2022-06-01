ff8ef871f5c819c1fa43683e7311d8a2cea64002
HUGB <arababs2015@gmail.com>
2022-06-01 18:21:14 +0100

    ALX JS 3

--- JS/3. Obj-Oriented-Programming/L2: Fxns at runtime/intro2.js
+++ JS/3. Obj-Oriented-Programming/L2: Fxns at runtime/intro2.js
@@ -21,9 +21,74 @@ Local variables declared within the function.
 Variables from its parent function's scope.
 Global variables.
 
+const myName = 'Andrew';
 
+function introduceMyself() {
+          const you = 'student';
 
+          function introduce() {
+                    console.log(`Hello, ${you}, I'm ${myName}!`);
+          }
 
+          return introduce();
+}
+
+introduceMyself();
+// 'Hello, student, I'm Andrew!'
+
+Or:
+
+const myName = 'Andrew';
+
+function introduceMyself() {
+          const you = 'student';
+
+          return function () {
+                    console.log(`Hello, ${you}, I'm ${myName}!`);
+          }
+
+}
+
+introduceMyself()();
+// 'Hello, student, I'm Andrew!'
+
+Q: Consider the following:
+
+const num1 = 5;
+
+function functionOne() {
+          const num2 = 10;
+
+          function functionTwo(num3) {
+                    const num4 = 35;
+
+                    return num1 + num2 + num3 + num4;
+          }
+
+          return functionTwo(0);
+}
+
+Which variables does functionTwo() have access to? 
+Select all that apply:
+
+All four variables are available for functionTwo() to use. 
+Let's break this one down:
+
+num1 is a global variable, accessible anywhere in the 
+application code.
+
+num2 is a local variable in functionOne(), the parent function 
+in which functionTwo() is defined.
+
+num3 is an argument passed directly to functionTwo().
+
+num4 is a local variable in functionTwo().
+
+JavaScript is Function-Scoped
+Any variables defined inside a function are not available 
+outside of that function. On the other hand, if there are any 
+variables defined inside a block (e.g., within an if statement), 
+those variables are available outside of that block.
 
 
 */ 
