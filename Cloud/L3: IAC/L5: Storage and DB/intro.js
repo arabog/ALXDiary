39e2655f6ed78553f4ddf1e3acc774684b6a299c
HUGB <arababs2015@gmail.com>
2022-06-02 14:37:04 +0100

    db update 2

--- Cloud/L3: IAC/L5: Storage and DB/intro.js
+++ Cloud/L3: IAC/L5: Storage and DB/intro.js
@@ -45,7 +45,48 @@ certainly possible! Also note-worthy: DynamoDB works but it
 is a document store and not a relational database, which is 
 great and sufficient for most cases but not all.
 
-go over rds part one
+Connectivity
+Subnet groups are needed for deploying in multiple AZs.
+
+We want to place our RDS in more than one Availability Zone 
+(data center). We can place the RDS in two AZs to eliminate 
+single point of failure and to have high availability.
+
+We created 4 subnets (2 private and 2 public), so the RDS can 
+potentially be duplicated in all four subnets.
+
+However, keep in mind that we usually prefer to put databases 
+in a private subnet, for security. There may be use cases where 
+you put a database in a public subnet but generally put it in the 
+private subnets.
+
+Database Accessibility
+Usually, don't make a database public.
+
+We'll choose "No" for public accessibility" to keep a database private.
+We'd normally use a private IP address to access a database.
+
+VPC Security Groups
+Default means every resource in the VPC can talk to any other 
+resource that is within that same VPC. We'll keep this default, to 
+allow resources in the VPC to reach the database.
+
+Encryption
+We can use encryption for sensitive production workloads. We can 
+disable encryption for our database here.
+
+RDS Running cost notice
+Be sure to delete any RDS databases that you don’t need once you 
+are done practicing because it can be very expensive if you forget 
+it and leave it running for a while!
+
+Q: Your database can only be accessed from other resources that 
+have the appropriate ____ assigned to them.
+Security Group
+
+Accessing server and services via TCP ports is a key networking 
+concept that is governed by Security Groups within your private 
+cloud.
 
 
 
