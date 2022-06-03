68825217881bc9c12af72a85559e0b0c407c4a6a
HUGB <arababs2015@gmail.com>
2022-06-03 07:55:22 +0100

    JS classes 3

--- JS/3. Obj-Oriented-Programming/L3: Classes and Objects/intro.js
+++ JS/3. Obj-Oriented-Programming/L3: Classes and Objects/intro.js
@@ -24,7 +24,33 @@ function SoftwareDeveloper() {
           this.favoriteLanguage = 'JavaScript';
 }
 
+First, rather than declaring local variables, constructor functions 
+persist data with the this keyword. The above function will add a 
+favoriteLanguage property to any object that it creates, and assigns 
+it a default value of 'JavaScript'. Don't worry too much about this 
+in a constructor function for now; just know that this refers to the 
+new object that was created by using the new keyword in front of 
+the constructor function.
 
+One last thing that might seem unusual is that this function 
+doesn't seem to return anything! Constructor functions in 
+JavaScript should not have an explicit return value (i.e., 
+there should not be return statement).
+
+Creating a New Object
+As we've seen above, let's use the new operator to create a new object:
+
+let developer = new SoftwareDeveloper();
+We've saved the return value of this invocation to the variable developer. 
+Let's execute console.log(developer);
+
+function SoftwareDeveloper() {
+          this.favoriteLanguage = 'JavaScript';
+}
+
+let developer = new SoftwareDeveloper();
+
+console.log(developer)        // SoftwareDeveloper {favoriteLanguage: 'JavaScript'}
 
 
 
