6ab952c49be4a059c3e312a9e77541f75cb6d695
HUGB <arababs2015@gmail.com>
2022-06-03 07:47:22 +0100

    JS classes 2

--- JS/3. Obj-Oriented-Programming/L3: Classes and Objects/intro.js
+++ JS/3. Obj-Oriented-Programming/L3: Classes and Objects/intro.js
@@ -2,6 +2,29 @@
 -: Properties and Methods
 classes (i.e., categories) of objects!
 
+-: Constructor Functions
+Previously, we have created objects using the object literal notation. 
+Likewise, we can even write functions that return objects. There is 
+yet another way for us to create objects, and it is the foundation of 
+object-oriented JavaScript: the constructor function. 
+
+To instantiate (i.e., create) a new object, we use the new operator 
+to invoke the function:
+new SoftwareDeveloper();
+
+The first thing to note above is the use of the new keyword. Second, 
+note that the name of the constructor function, SoftwareDeveloper(), 
+is written with the first letter capitalized to visually distinguish it 
+from a regular function.
+
+Constructor Functions: Structure and Syntax
+This is what the internals of a constructor function looks like:
+
+function SoftwareDeveloper() {
+          this.favoriteLanguage = 'JavaScript';
+}
+
+
 
 
 
