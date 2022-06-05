b7a105b8c6874ad228438d4a6c1d1bb7bf96b04a
HUGB <arababs2015@gmail.com>
2022-06-05 20:14:04 +0100

    Last L3 section   1

---
+++ JS/3. Obj-Oriented-Programming/L3: Classes and Objects/intro3.js
@@ -0,0 +1,13 @@
+/*
+-: Prototypal Inheritance: Subclasses
+Subclasses
+One of the benefits of implementing inheritance is that it 
+allows you to reuse existing code. By establishing inheritance, 
+we can subclass, that is, have a "child" object take on most or 
+all of a "parent" object's properties while retaining unique 
+properties of its own.
+
+
+
+
+*/ 
